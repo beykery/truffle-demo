420f02af2527baf631a329fa597091f49abb25b4
yangchaoqun <yangchaoqun@huobi.com>
2020-11-09 19:04:37 +0800

    fix

--- contracts/interfaces/ILiquidityValueCalculator.sol
+++ contracts/interfaces/ILiquidityValueCalculator.sol
@@ -1,5 +1,5 @@
-pragma solidity ^0.6.6;
-
-interface ILiquidityValueCalculator {
-    function computeLiquidityShareValue(uint liquidity, address tokenA, address tokenB) external returns (uint tokenAAmount, uint tokenBAmount);
-}
+//pragma solidity ^0.5.0;
+//
+//interface ILiquidityValueCalculator {
+//    function computeLiquidityShareValue(uint liquidity, address tokenA, address tokenB) external returns (uint tokenAAmount, uint tokenBAmount);
+//}
